bb42daca2c9cfc24aa6400ce2a2aa9494253dfdf
Devavarshita <devavarshita2311@gmail.com>
2026-08-30 13:34:34 +0530

    Update certifications.js

--- data/certifications.js
+++ data/certifications.js
@@ -1,42 +1,61 @@
 export const certifications = [
-  /*{
-     title: "AWS Certified Solutions Architect – Associate",
+  {
+    title: "AWS Certified Solutions Architect – Associate",
     issuer: "Amazon Web Services (AWS)",
-    date: "2026",
+    year: "2026",
+    icon: "☁️",
+    description:
+      "Globally recognized certification validating the ability to design secure, scalable, resilient, and cost-optimized solutions on AWS.",
     link: "https://www.credly.com/badges/840f1ca1-664e-4334-bab4-fd75ef1b21a7/public_url",
   },
 
   {
     title: "AWS Certified Cloud Practitioner",
     issuer: "Amazon Web Services (AWS)",
-    date: "2026",
+    year: "2026",
+    icon: "☁️",
+    description:
+      "Globally recognized certification validating foundational knowledge of AWS Cloud services, architecture, security, and cloud concepts.",
     link: "https://www.credly.com/badges/73075a09-680d-4586-90b5-05c737725559/public_url",
-  },*/
+  },
 
   {
     title: "ServiceNow Certified System Administrator",
-issuer: "ServiceNow",
-date: "2026",
-link: "https://www.credly.com/badges/e199e776-f698-4060-925c-b2d4bc36f6f0/public_url",
+    issuer: "ServiceNow",
+    year: "2026",
+    icon: "⚙️",
+    description:
+      "Industry-recognized certification validating knowledge of ServiceNow platform administration, configuration, security, and core platform capabilities.",
+    link: "https://www.credly.com/badges/e199e776-f698-4060-925c-b2d4bc36f6f0/public_url",
   },
- 
+
   {
     title: "Java Programming Fundamentals",
     issuer: "Infosys Springboard",
-    date: "2024",
-    link: "https://drive.google.com/file/d/1dpktH0YQpuKDzx0Saeknm2-61pD2ux79/view?usp=sharing", 
+    year: "2024",
+    icon: "☕",
+    description:
+      "Strengthened core Java programming skills, object-oriented programming concepts, and problem-solving fundamentals.",
+    link: "https://drive.google.com/file/d/1dpktH0YQpuKDzx0Saeknm2-61pD2ux79/view?usp=sharing",
   },
+
   {
     title: "Developing Front-End Applications with React",
     issuer: "IBM",
-    date: "2025",
+    year: "2025",
+    icon: "⚛️",
+    description:
+      "Built knowledge of developing modern, component-based, interactive web applications using React.",
     link: "https://drive.google.com/file/d/1AkdqQv0P45h5tQSC_i53yYSPQIKb7po5/view?usp=sharing",
   },
+
   {
     title: "Getting Started with Git and GitHub",
     issuer: "IBM",
-    date: "2025",
+    year: "2025",
+    icon: "🔗",
+    description:
+      "Developed practical knowledge of version control, repository management, collaboration workflows, and GitHub.",
     link: "https://drive.google.com/file/d/1Ni9o_uA6g6iKkfPg-AF_Z1zfVkgUrfL8/view?usp=sharing",
   },
 ];
-  
